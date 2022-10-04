0e89f186e0b9383e79c56187945e74238734419a
kleber <kleberjrmmxvi@gmail.com>
2022-10-04 19:11:35 -0300

    FALTANDO 'LIGAR' OS INPUTS COM A INTERFACE

--- pysimplegui_interface.py
+++ pysimplegui_interface.py
@@ -23,41 +23,43 @@ def main():
 if __name__=='__main__':
     main()
 
+
 ##TKINTER
 
 janela = tkinter.Tk()
 
 janela.title('Bolsa de valores')
 
+
 input_data = tkinter.Label(janela, text="DATA")
-input_data.grid(column=0, row=0)
+input_data.grid(column=0, row=0, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="CODIGO")
-input_data.grid(column=0, row=1)
+input_data.grid(column=0, row=1, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="QUANTIDADE")
-input_data.grid(column=0, row=2)
+input_data.grid(column=0, row=2, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="VALOR UNITÁRIO")
-input_data.grid(column=0, row=3)
+input_data.grid(column=0, row=3, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="TIPO OPERAÇÃO")
-input_data.grid(column=0, row=4)
+input_data.grid(column=0, row=4, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="VALOR OPERAÇÃO(SEM TAXAS)")
-input_data.grid(column=0, row=5)
+input_data.grid(column=0, row=5, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="CORRETAGEM")
-input_data.grid(column=0, row=6)
+input_data.grid(column=0, row=6, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="TAXA B3")
-input_data.grid(column=0, row=7)
+input_data.grid(column=0, row=7, padx=10, pady=10)
 
 input_data = tkinter.Label(janela, text="VALOR DA OPERAÇÃO")
-input_data.grid(column=0, row=8)
+input_data.grid(column=0, row=8, padx=10, pady=10)
 
 
-botao = tkinter.Button(janela, text="Ver histórico", command=main)
-botao.grid(column=0, row=9)
+botao = tkinter.Button(janela, text="Ver histórico")
+botao.grid(column=0, row=9, padx=10, pady=10)
 
 janela.mainloop()
